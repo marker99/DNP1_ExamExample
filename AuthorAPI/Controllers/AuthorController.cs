6b60c5ca4ae9135810459acc262460bbdc50eb24
marker99 <frederik.m.hansen@gmail.com>
2021-11-23 14:52:17 +0100

    Done some more stuff

---
+++ AuthorAPI/Controllers/AuthorController.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using AuthorAPI.Models;
+using AuthorAPI.Persistence;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AuthorAPI.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class AuthorController
+    {
+        
+    }
+}
